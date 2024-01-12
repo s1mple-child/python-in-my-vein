c04ccac12e782cdb3234affd170a815c3ce9286c
ghetto child <simp1e.zhj@gmail.com>
2024-01-12 08:33:13 +0000

    fix duplicate code checked by pylint

--- gerrit/gerrit_get_files_from_change.py
+++ gerrit/gerrit_get_files_from_change.py
@@ -6,6 +6,7 @@ author: s1mple-child
 import json
 import sys
 import requests
+from migrate_from_gitlab_2gerrit import *
 
 
 # find changed files via gerrit REST API
@@ -34,57 +35,6 @@ def get_changed_files(gerrit_ip, project_name, branch_name, change_id, revision_
         files_list.append(key)
 
     print(files_list)
-
-
-# create http access session in gerrit
-def create_access_session(gerrit_ip):
-    """
-    create http access session for http request in gerrit
-    :param gerrit_ip: gerrit ip address
-    :return: session: a session to request with gerrit admin username and http credential
-    """
-    # set admin username and http credential in gerrit with different gerrit instances
-    gerrit_admin_username, gerrit_admin_http_cred = set_admin_username_n_http_cred(gerrit_ip)
-    # create session
-    session = requests.Session()
-    session.auth = (gerrit_admin_username, gerrit_admin_http_cred)
-
-    return session
-
-# in my use case, gerrit always have diffenent ports after deployed
-# set port number with given gerrit ip address
-def set_gerrit_port(gerrit_ip):
-    """
-    set gerrit port with given gerrit ip address
-    :param gerrit_ip: gerrit ip address
-    :return: gerrit_port: port number of gerrit
-    """
-    gerrit_port = ''
-    if gerrit_ip == 'gerrit.ip.addr.1':
-        gerrit_port = '1111'
-    elif gerrit_ip == 'gerrit.ip.addr.2':
-        gerrit_port = '2222'
-    return gerrit_port
-
-
-# set admin user name and http credential
-def set_admin_username_n_http_cred(gerrit_ip):
-    """
-    set gerrit system administrator username and http credential
-    :param gerrit_ip: gerrit ip address
-    :return: admin_user_name: username of gerrit system administrator
-    :return: admin_http_cred: http credential of this gerrit administrator
-    """
-    admin_user_name = ''
-    admin_http_cred = ''
-    if gerrit_ip == 'gerrit.ip.addr.1':
-        admin_user_name = 'user1'
-        admin_http_cred = 'http_cred1'
-    elif gerrit_ip == 'gerrit.ip.addr.2':
-        admin_user_name = 'user2'
-        admin_http_cred = 'http_cred2'
-    
-    return admin_user_name, admin_http_cred
 
 
 get_changed_files(sys.argv[1], sys.argv[2], sys.argv[3], sys.argv[4], sys.argv[5])
